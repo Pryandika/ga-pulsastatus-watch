175e037ea79cc313478ad241a923904253f53fb9
Pasek Ryandika <pryandika7@gmail.com>
2026-03-11 13:06:28 +0800

    feat: initial commit

---
+++ app/api/data/route.ts
@@ -0,0 +1,16 @@
+export async function GET() {
+  const res = await fetch(`${process.env.API_URL}/data`, {
+    headers: {
+      "x-api-key": process.env.API_KEY as string,
+    },
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    return new Response("API error", { status: 500 });
+  }
+
+  const data = await res.json();
+
+  return Response.json(data);
+}
