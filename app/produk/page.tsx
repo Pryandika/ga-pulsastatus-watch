52e0ec0df400d75fcf9cedde5a8c32a6329a9807
Pasek Ryandika <91819749+Pryandika@users.noreply.github.com>
2026-03-12 22:31:45 +0800

    Revert "Merge branch 'dev' of https://github.com/Pryandika/ga-pulsastatus-watch into dev"
    This reverts commit 060841e826900b503fffe16d6f5d118cd3b99ce0, reversing
    changes made to c61a2ed6631a13d3eee7433b3433ed557d7a8679.

--- app/produk/page.tsx
+++ app/produk/page.tsx
@@ -4,9 +4,9 @@ import { OperatorGroup } from "./types";
 export const revalidate = 300;
 
 export default async function ProdukPage() {
-  const res = await fetch("https://api.gunaarthapulsa.com/data", {
+  const res = await fetch(`${process.env.API_URL}`, {
     headers: {
-      "x-api-key": "LaFvdfXAZdM2XPn0vPUrD1RLdpcpWHj1",
+      "x-api-key": process.env.API_KEY as string,
     },
     next: { revalidate: 300 },
   });
